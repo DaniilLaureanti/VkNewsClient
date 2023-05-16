fb7bbb44d48dfb6921b03660523dbd757bf597df
Laureanti Daniil <daniil.laureanti@gmail.com>
2023-05-16 11:31:28 +0200

    Add data layer 15.05.2023

--- app/src/main/java/com/laureanti/vknewsclient/presentation/news/NewsFeedViewModel.kt
+++ app/src/main/java/com/laureanti/vknewsclient/presentation/news/NewsFeedViewModel.kt
@@ -1,22 +1,41 @@
 package com.laureanti.vknewsclient.presentation.news
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.laureanti.vknewsclient.data.mapper.NewsFeedMapper
+import com.laureanti.vknewsclient.data.network.ApiFactory
 import com.laureanti.vknewsclient.domain.FeedPost
 import com.laureanti.vknewsclient.domain.StatisticItem
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
+import com.vk.api.sdk.auth.VKAccessToken
+import kotlinx.coroutines.launch
 
-class NewsFeedViewModel : ViewModel() {
+class NewsFeedViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val sourceList = mutableListOf<FeedPost>().apply {
-        repeat(10) {
-            add(FeedPost(id = it))
-        }
-    }
-    private val initialState = NewsFeedScreenState.Posts(posts = sourceList)
+    private val initialState = NewsFeedScreenState.Initial
 
     private val _screenState = MutableLiveData<NewsFeedScreenState>(initialState)
     val screenState: LiveData<NewsFeedScreenState> = _screenState
+
+    private val mapper = NewsFeedMapper()
+
+    init {
+        loadRecommendations()
+    }
+
+    private fun loadRecommendations() {
+        viewModelScope.launch {
+            val storage = VKPreferencesKeyValueStorage(getApplication())
+            val token = VKAccessToken.restore(storage) ?: return@launch
+            val response = ApiFactory.apiService.loadRecommendations(token.accessToken)
+            val feedPost = mapper.mapResponseToPost(response)
+            _screenState.value = NewsFeedScreenState.Posts(posts = feedPost)
+        }
+    }
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem) {
         val currentState = screenState.value
--- app/src/main/java/com/laureanti/vknewsclient/ui/theme/Color.kt
+++ app/src/main/java/com/laureanti/vknewsclient/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val Black900 = Color(0xFF161515)
 val Black500 = Color(0xFF8C9191)
 val DarkBlue = Color(0xff4c75a3)
+val DarkRed = Color(0xfffb3043)
